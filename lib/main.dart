d15c128ec7d2c265e45defd2d861038dde2765a4
Sushil Jacob <sushjacob3@gmail.com>
2026-02-10 12:23:25 -0500

    Finished switch screen logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: StartScreen(),
+      body: Quiz(),
     )
   ));
 }
