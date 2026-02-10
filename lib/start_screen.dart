5c80047eb99af42184e441a11b23be7d9bcb4a0a
Sushil Jacob <sushjacob3@gmail.com>
2026-02-10 11:27:45 -0500

    Finished intial start_screen method

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -14,7 +14,30 @@ class StartScreen extends StatelessWidget{
           end:Alignment.bottomRight,
           )
         ),
-        child:Center(child: Text("Start Quiz"),)
+        child:Center(
+          child: Column(
+            mainAxisSize:MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 280,
+              ),
+              const SizedBox(
+                height: 80,
+                ),
+              Text(
+                "Challenge yourself on this quiz!",
+                style: TextStyle(color: Colors.white, fontSize:22),
+              ),
+              const SizedBox(height: 30),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+                child: Text("Start quiz"),
+              ),
+            ],
+          ),
+          ),
       );
     }
 }
