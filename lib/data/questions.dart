55f44dd5acceb1e05bb4f98c4fa7617f4fc70e76
Sushil Jacob <sushjacob3@gmail.com>
2026-02-12 14:31:06 -0500

    Created quiz questions and answers

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -3,21 +3,57 @@ import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 const questions = [
 QuizQuestion(
-  'How did Edward Elric lose his arm in FMA?',
+  'Which player has won the most NBA MVP awards?',
   [
-    'Putting his brothers sould in a suit of armor',
-    'In a Duel',
-    'In a freak Dabbing Accident',
-    'He never had any arms',
+    'Micheal Jordan',
+    'Lebron James',
+    'Kareem Abdul-Jabbar', //correct
+    'Bill Russell',
   ]
   ),
   QuizQuestion(
-    'What is the capital of Japan?',
+    'Who holds the record for most points scored in NBA history?',
     [
-      'Seoul', //Correct
-      'Tokyo',
-      'Beijing',
-      'Bangkok',
+      'Kobe Bryant', 
+      'Lebron James', //Correct
+      'Kareem-Abdul Jabbar',
+      'Micheal Jordan',
+    ]
+  ),
+  QuizQuestion(
+    'Which team has won the most NBA championships?',
+    [
+      'Los Angeles Lakers', 
+      'Boston Celtics', //Correct
+      'Chicago Bulls',
+      'Golden State Warriors',
+    ]
+  ),
+  QuizQuestion(
+    'Who scored 100 points in a single NBA game?',
+    [
+      'Kobe Bryant',
+      'Micheal Jordan', 
+      'Wilt Chamberlain', //correct
+      'Elgin Baylor',
+    ]
+  ),
+  QuizQuestion(
+    'How many NBA championships did Michael Jordan win with the Chicago Bulls?',
+    [
+      '5', 
+      '6', //correct
+      '7',
+      '8',
+    ]
+  ),
+  QuizQuestion(
+    'Who is the NBAs all-time leader in assists?',
+    [
+      'Magic Johnson',
+      'Steve Nash',
+      'Chris Paul',
+      'John Stockton', //correct
     ]
   ),
 
