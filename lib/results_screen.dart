6d20d5490b30a710c7d0a0f2311846402a8d990c
Sushil Jacob <sushjacob3@gmail.com>
2026-03-05 12:17:57 -0500

    Created questions_summary widget and loaded on screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 class ResultsScreen extends StatelessWidget{
   const ResultsScreen({super.key,
    required this.chosenAnswers});
 
 final List<String> chosenAnswers;
+
+List<Map<String, Object>> getSummaryData(){
+  List<Map<String, Object>> summary =[];
+  for(var i = 0; i < chosenAnswers.length;i++){
+    summary.add({
+      'question_index':i,
+      'question':questions[i].question,
+      'correct_answer':questions[i].answers[0],
+      'user_answer':chosenAnswers[i]
+    });
+  }
+  return summary;
+}
+
 @override
   Widget build(context)
   {
+    final summaryData = getSummaryData();
+    final materialQuestions = getSummaryData().length;
+    final numTotalCorrect = summaryData.where(
+      (data){
+        return data['correct_answer:'] == data['user_answer'];
+      } 
+      ).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -16,6 +40,7 @@ final List<String> chosenAnswers;
           children:  [
             const Text('You answered X out of Y questions Correctly'),
             const SizedBox(height:30,),
+            QuestionSummary(summaryData: summaryData),
             const Text('List of Answers and Questions'),
             const SizedBox(height:30,),
             TextButton(
